a1ddb660588162cf8d7a3e24881ff1a73b2b5327
MotionControls <bprogers43@tntech.edu>
2023-10-17 11:37:58 -0500

    Init commit Sprite_O

---
+++ source/sprites/Sprite_O.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Sprite_O)
+
+//======================================================================
+//
+//	Sprite_O, 16x16@4, 
+//	+ palette 8 entries, not compressed
+//	+ 4 tiles Metatiled by 2x2 not compressed
+//	Total size: 16 + 128 = 144
+//
+//	Time-stamp: 2023-10-17, 11:25:51
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SPRITE_O_H
+#define GRIT_SPRITE_O_H
+
+#define Sprite_OTilesLen 128
+extern const unsigned short Sprite_OTiles[64];
+
+#define Sprite_OPalLen 16
+extern const unsigned short Sprite_OPal[8];
+
+#endif // GRIT_SPRITE_O_H
+
+//}}BLOCK(Sprite_O)
